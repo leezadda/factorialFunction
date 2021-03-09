6be33dc14e34de0cac6a3b119e0c72da6344e914
Leo Quezada <74736115+leezadda@users.noreply.github.com>
2021-03-08 19:51:00 -0600

    Add files via upload

---
+++ 11111111.py
@@ -0,0 +1,10 @@
+
+
+
+
+m = int(input("Enter the value of m: "))
+n = int(input("Enter the value of n: "))
+sum = 0
+for i in range(m, n + 1):
+    sum = sum + i
+print("The sum of the natural numbers from", m, "to", n, "is:", sum)
